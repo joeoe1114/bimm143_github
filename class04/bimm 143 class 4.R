9f2be282d0cdbad4302793bb3328418eb7d931d4
jhw008@ucsd.edu <joannewu@Joannes-MacBook-Air.local>
2024-05-21 11:54:40 -0700

    Add class4

---
+++ class04/bimm 143 class 4.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4: R basics"
+#' author: "Joanne Wu (PID: A17060517)"
+#' date: "Apr 11th, 2024"
+#' ---
+
+# this is just a text file
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="turquoise", lwd=3)
